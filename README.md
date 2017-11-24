ad91d4486d63308e8876b37e7b7e9193ffb95ae9
JelteMX <jelte.lagendijk@mendix.com>
2017-11-24 15:43:51 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-mendix-widget-es-6 [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Generate Mendix Widgets in ES6 style
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-mendix-widget-es-6 using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-mendix-widget-es-6
+```
+
+Then generate your new project:
+
+```bash
+yo mendix-widget-es-6
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [JelteMX]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-mendix-widget-es-6.svg
+[npm-url]: https://npmjs.org/package/generator-mendix-widget-es-6
+[travis-image]: https://travis-ci.org/JelteMX/generator-mendix-widget-es-6.svg?branch=master
+[travis-url]: https://travis-ci.org/JelteMX/generator-mendix-widget-es-6
+[daviddm-image]: https://david-dm.org/JelteMX/generator-mendix-widget-es-6.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/JelteMX/generator-mendix-widget-es-6
+[coveralls-image]: https://coveralls.io/repos/JelteMX/generator-mendix-widget-es-6/badge.svg
+[coveralls-url]: https://coveralls.io/r/JelteMX/generator-mendix-widget-es-6
---
+++ generators/app/index.js
@@ -0,0 +1,127 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const pkg = require('../../package.json');
+const extend = require('deep-extend');
+const {getBanner} = require('../lib/text');
+
+module.exports = class extends Generator {
+  initializing() {
+    this.props = {};
+  }
+
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(getBanner(pkg));
+
+    const prompts = [
+      {
+        name: 'packageName',
+        message: 'Package name (part of Package.widget.WidgetName)',
+        default: 'Package',
+        when: !this.props.packageName
+      },
+      {
+        name: 'widgetName',
+        message: 'Widget name (part of Package.widget.WidgetName)',
+        default: 'Widget',
+        when: !this.props.widgetName
+      },
+      {
+        name: 'friendlyWidgetName',
+        message: 'Friendly widget name (in Modeler)',
+        default: this.props.widgetName,
+        when: !this.props.friendlyWidgetName
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  _copySourceFile(path) {
+    this.fs.copy(this.templatePath(`widget-base/${path}`), this.destinationPath(path));
+  }
+
+  writing() {
+    const {
+      packageName,
+      widgetName,
+      friendlyWidgetName
+    } = this.props;
+
+    this._copySourceFile('.babelrc');
+    this._copySourceFile('.editorconfig');
+    this._copySourceFile('.eslintignore');
+    this._copySourceFile('.eslintrc');
+    this._copySourceFile('.gitignore');
+    this._copySourceFile('Gulpfile.js');
+    this._copySourceFile('postcss.config.js');
+    this._copySourceFile('webpack.config.js');
+    this._copySourceFile('widgetpackage.template.xml.ejs');
+
+    this.fs.copy(
+      this.templatePath(`widget-base/src/Widget/Widget.xml`),
+      this.destinationPath(`src/${packageName}/${widgetName}.xml`),
+      {
+        process: file => {
+          let fileText = file.toString();
+          fileText = fileText
+            .replace(/Widget\.widget\.Widget/g, `${packageName}.widget.${widgetName}`)
+            .replace(/<name>Widget<\/name>/g, `<name>${friendlyWidgetName}</name>`);
+          return fileText;
+        }
+      }
+    );
+
+    this.fs.copy(
+      this.templatePath(`widget-base/src/Widget/widget/Widget.js`),
+      this.destinationPath(`src/${packageName}/widget/${widgetName}.js`),
+      {
+        process: file => {
+          let fileText = file.toString();
+          fileText = fileText
+            .replace(/'Widget'/g, `'${widgetName}'`);
+          return fileText;
+        }
+      }
+    );
+
+    this.fs.copy(
+      this.templatePath(`widget-base/src/Widget/widget/Widget.scss`),
+      this.destinationPath(`src/${packageName}/widget/${widgetName}.scss`)
+    );
+
+    this.fs.copy(
+      this.templatePath(`widget-base/src/Widget/widget/Widget.template.html`),
+      this.destinationPath(`src/${packageName}/widget/${widgetName}.template.html`),
+    );
+
+    this.fs.copy(
+      this.templatePath(`widget-base/src/Widget/widget/Core.js`),
+      this.destinationPath(`src/${packageName}/widget/Core.js`)
+    );
+    this.fs.copy(
+      this.templatePath(`widget-base/src/Widget/widget/Libraries.js`),
+      this.destinationPath(`src/${packageName}/widget/Libraries.js`)
+    );
+
+    const source = this.fs.readJSON(this.templatePath(`widget-base/package.json`));
+
+    extend(source, {
+      widget: {
+        package: packageName,
+        libraries: false,
+        core: false,
+        path: false
+      }
+    });
+
+    this.fs.writeJSON(this.destinationPath('package.json'), source);
+  }
+
+  install() {
+    this.installDependencies({bower: false});
+  }
+};
